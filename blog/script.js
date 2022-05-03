b492f47497828c3d3ff3443a8a6cc73a4b51b4bc
Glitch (glitch-blank-website) <none>
2022-05-03 22:18:13 +0000

    🍓⛸ Checkpoint ./blog/script.js:31343558/742 ./blog/2/index.html:31343558/17 ./blog/1/index.html:31343558/874 ./blog/3/index.html:31343558/879

--- blog/script.js
+++ blog/script.js
@@ -3,5 +3,13 @@ function previous() {
   var x = window.location.pathname.split('/')
   var y = x[2]
   var z = y-1
-  window.location.href(z)
+  window.location.replace('https://turkey2098.co.vu/blog/' + z)
+}
+
+function next() {
+  var x = window.location.pathname.split('/')
+  var y = x[2]
+  var z = y-y
+  var a = (z+(1*y))+1
+  window.location.replace('https://turkey2098.co.vu/blog/' + a)
 }
