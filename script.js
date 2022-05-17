4c3daffc58613db5bec1e99954f7c4f7fd4badfd
Glitch (turkey-2098) <none>
2022-05-17 19:55:17 +0000

    🕵👀 Checkpoint ./test.html:31343558/13215 ./script.js:31343558/184 ./style.css:31343558/1961 ./index.html:31343558/518 ./0toDo.txt:31343558/8

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 var counter = document.getElementById("counter");
 counter.style.display="none";
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
