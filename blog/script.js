1af8940c8d75323c6c573cc65e049482da32cc74
Glitch (turkey-2098) <none>
2022-05-18 12:32:23 +0000

    👨‍❤️‍💋‍👨👕 Checkpoint ./blog/script.js:31343558/118

--- blog/script.js
+++ blog/script.js
@@ -9,6 +9,7 @@ function previous() {
 function next() {
   var x = window.location.pathname.split('/')
   var y = x[2]
-  var z = y+1
-  window.location.replace(z)
+  var z = y-y
+  var a = (z+(1*y))+1
+  window.location.replace(a)
 }
